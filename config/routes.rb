6f308ba9838159c67e3d483f1013c19c68a69303
Archana <app59@njit.edu>
2016-04-04 18:00:32 +0000

    assertions

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
 
 delete 'logout', to: 'sessions#destroy'
+  
+  resources :categories,except: [:destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
